f8e4bdedf38f4fa91def7764c5c0db21b35dfc05
竹内和人 <takeuchikazuto@takeuchikazutonoMacBook-Pro.local>
2023-12-18 14:48:41 +0900

    fix

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials[:front_domain] || 'localhost:3000'
+    origins Rails.application.credentials[:front_domain] 
 
     resource '*',
              headers: :any,
