e3cff2f9a35e4c6e08200c8f183f015368ce3f27
竹内和人 <takeuchikazuto@takeuchikazutonoMacBook-Pro.local>
2023-12-24 23:02:42 +0900

    fix

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -93,5 +93,4 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
   }
-
 end
--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -10,7 +10,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    origins Settings.front_domain.split(',')
+    origins Settings.front_domain.split(",")
 
     resource "*",
              headers: :any,
